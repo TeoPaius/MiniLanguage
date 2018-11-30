ff0a0b00ee51bb27863c368b174a00db5d8e3269
Teo <teodor_paius@yahoo.com>
2018-11-30 14:54:37 +0200

    first initialized

--- llparser/LLParser.py
+++ llparser/LLParser.py
@@ -1,3 +1,27 @@
 class LLParser:
     def __init__(self):
         pass
+
+
+    def createFirst(self, grammar):
+        res = {}
+        for i in grammar.N:
+            res[i] = []
+
+        for terminal in grammar.E:
+            res[terminal] = [terminal]
+
+        i = 0
+        for prod in grammar.P:
+            if prod.right[0] in grammar.E:
+                res[prod.left][i] = prod.right[0]
+
+
+    def createFollow(self, first, grammar):
+        pass
+
+    def createTable(self, first, follow, grammar):
+        pass
+
+    def analyseSeq(self, table, sequence):
+        pass
