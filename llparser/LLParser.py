c20b7fe08ca3000158ab352c392ebe958be1431e
Dorinel Panaite <van.panaite@gmail.com>
2018-11-30 15:01:44 +0200

    Fixed merge problems

--- llparser/LLParser.py
+++ llparser/LLParser.py
@@ -2,29 +2,33 @@ from llparser.Grammar import Grammar
 
 
 class LLParser:
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.grammar = Grammar(filename)
 
-
-    def createFirst(self, grammar):
+    def create_first(self):
         res = {}
-        for i in grammar.N:
+        for i in self.grammar.N:
             res[i] = []
 
-        for terminal in grammar.E:
+        for terminal in self.grammar.E:
             res[terminal] = [terminal]
 
         i = 0
-        for prod in grammar.P:
-            if prod.right[0] in grammar.E:
+        for prod in self.grammar.P:
+            if prod.right[0] in self.grammar.E:
                 res[prod.left][i] = prod.right[0]
 
-
-    def createFollow(self, first, grammar):
+    def create_follow(self, first, grammar):
         pass
 
-    def createTable(self, first, follow, grammar):
+    def create_table(self, first, follow, grammar):
         pass
 
-    def analyseSeq(self, table, sequence):
+    def analyse_seq(self, table, sequence):
         pass
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return str(self.grammar)
